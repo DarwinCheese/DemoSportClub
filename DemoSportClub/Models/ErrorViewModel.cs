f22b90febdbfd86035114eba432d375b30ba07b3
Darwin Gutierrez <Darwincheese@gmail.com>
2020-02-04 15:15:18 +0100

    Add project files.

---
+++ DemoSportClub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoSportClub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
